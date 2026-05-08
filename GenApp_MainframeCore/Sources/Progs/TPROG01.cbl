487643c2063d0a1548a7d41522457c03566723a0
neil <neil_gilford@bmc.com>
2026-05-08 10:13:42 +1000

    pgm change

--- GenApp_MainframeCore/Sources/Progs/TPROG01.cbl
+++ GenApp_MainframeCore/Sources/Progs/TPROG01.cbl
@@ -8,7 +8,7 @@
 000800* THIS PROGRAM IS A TRAINING PROGRAM
 000900*      WITH A LINK TO TSUBR01
 001000*  **  May 8th
-      * 8th May again
+      * 8th May again and again
 001100*      AND COPYLIBS   TCPYA01   (HEADER)
 001200*                     TCPYB01   (LINK AREA)
 001300* ======================================================
